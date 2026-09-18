8287591b66490d7cda7b0e9106366b1a119c00dd
Tobias Wirth <25346708+dunahan@users.noreply.github.com>
2026-09-18 11:18:16 +0200

    Update model names in process_model_inner call

--- src/main.rs
+++ src/main.rs
@@ -796,7 +796,7 @@ mod tests {
             debug: false,
         };
 
-        process_model_inner(&src, "pmh0_test", "pma0_test", &out, &transform, &options).unwrap();
+        process_model_inner(&src, "pmh0_robe112", "pma0_robe112", &out, &transform, &options).unwrap();
 
         let written = fs::read_to_string(&out).unwrap();
         assert!(
