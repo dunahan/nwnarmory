416a47951414078727afabca7194bf8279eace26
Tobias Wirth <25346708+dunahan@users.noreply.github.com>
2026-09-18 10:26:45 +0200

    Format check

--- src/main.rs
+++ src/main.rs
@@ -862,8 +862,7 @@ mod tests {
             debug: false,
         };
 
-        process_model_inner(&src, "pmh0_test", "pma0_test", &out, &transform, &options)
-        .unwrap();
+        process_model_inner(&src, "pmh0_test", "pma0_test", &out, &transform, &options).unwrap();
 
         let written = fs::read_to_string(&out).unwrap();
         let positions: Vec<&str> = written
